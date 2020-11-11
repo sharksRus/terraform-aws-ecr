e656dce827a6d709167d237504791e59debb83fe
Glen Stummer <glen@glen.dev>
2020-11-10 20:35:05 -0600

    fmt and yamllint

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "repo_name" {
-    description = "Name of your ECR Repo"
+  description = "Name of your ECR Repo"
 }
 variable "scan_on_push" {
-    description = "Enable vulnerability scanning on ECR repo"
-    default = true
+  description = "Enable vulnerability scanning on ECR repo"
+  default     = true
 }
