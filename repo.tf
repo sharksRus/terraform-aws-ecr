53ae4f3724a87535d079628b1207397451b74f3d
vglen <vglen@users.noreply.github.com>
2021-06-11 13:46:31 -0500

    updating gha and ecr repo items (#3)
    * updating gha and ecr repo items
    
    * tfsec and tflint work

--- repo.tf
+++ repo.tf
@@ -1,8 +1,13 @@
 resource "aws_ecr_repository" "repo" {
-  name                 = var.repo_name
-  image_tag_mutability = "MUTABLE"
-
+  name = var.repo_name
+  encryption_configuration { #tfsec:ignore:AWS093
+    count           = var.enable_kms ? 1 : 0
+    encryption_type = var.encryption_type
+    kms_key         = var.kms_key_arn
+  }
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
   }
+  image_tag_mutability = "MUTABLE" #tfsec:ignore:AWS078
+  tags                 = merge(local.defaultTags, tomap({ "Name" = "${var.repo_name}" }))
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,28 @@
+variable "enable_kms" {
+  type        = bool
+  description = "True for KMS, false for AES256"
+  default     = false
+}
+variable "encryption_type" {
+  description = "Type of encryption for the repository"
+  default     = "AES256"
+}
+variable "kms_key_arn" {
+  description = "The arn of the kms key is not using default of AES256"
+  default     = ""
+}
 variable "life_cycle_policy_days" {
   description = "The humber of days to hold untagged images"
   default     = 90
 }
-
 variable "life_cycle_policy_tagstatus" {
   description = "The status of the image tag"
   default     = "untagged"
+}
+variable "principals_pull_access" {
+  type        = list(string)
+  description = "Principal ARNs to provide with pull access to the ECR"
+  default     = []
 }
 variable "repo_name" {
   description = "Name of your ECR Repo"
