9dbd266f3376b5abb455253e0348124a469df2e7
Glen Stummer <glen@glen.dev>
2023-07-01 16:15:48 -0500

    [ECR-1] tfsec/tflint fixes

--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
 variable "app" {
+  type        = string
   description = "Application"
   default     = "ecr"
 }
 variable "buildmethod" {
+  type        = string
   description = "IaC used"
   default     = "terraform"
 }
@@ -15,9 +17,11 @@ variable "encryption_configuration" {
   default     = null
 }
 variable "env" {
+  type    = string
   default = ""
 }
 variable "encryption_type" {
+  type        = string
   description = "Type of encryption for the repository"
   default     = "AES256"
 }
@@ -32,22 +36,27 @@ variable "force_delete" {
   desscription = "will delete the repository even if it contains images, set to false to disable"
 }
 variable "kms_key_arn" {
+  type        = string
   description = "The arn of the kms key is not using default of AES256"
-  default     = ""
+  default     = null
 }
 variable "life_cycle_policy_days" {
+  type        = number
   description = "The humber of days to hold untagged images"
   default     = 90
 }
 variable "life_cycle_policy_tagstatus" {
+  type        = string
   description = "The status of the image tag"
   default     = "untagged"
 }
 variable "org" {
+  type        = string
   description = "Namme used for the tag keys"
   default     = "my_org"
 }
 variable "owner" {
+  type        = string
   description = "Repository Owner"
   default     = ""
 }
@@ -57,17 +66,16 @@ variable "principals_pull_access" {
   default     = []
 }
 variable "repo_name" {
+  type        = string
   description = "Name of your ECR Repo"
 }
 variable "scan_on_push" {
+  type        = bool
   description = "Enable vulnerability scanning on ECR repo"
   default     = true
 }
-variable "tags" {
-  type    = any
-  default = []
-}
 variable "technicalcontact" {
+  type        = string
   description = "Repository Technical Contact Email"
   default     = "me@example.com"
 }
